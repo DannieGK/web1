b9dda74000c4db16b6a45c0dec3be8f3d9eb9ea2
DannieGK <kimgoeun@kakao.com>
2019-11-18 22:00:09 +0900

    add jquery

---
+++ colors.js
@@ -0,0 +1,49 @@
+var Body = {
+  setBackgroundColor:function (color){
+    document.querySelector('body').style.backgroundColor = color;
+  },
+  setColor:function (color){
+    document.querySelector('body').style.color = color;
+  }
+}
+var Links = {
+  setColor:function(color){
+
+  // // jquery 사용전 반복문 사용.
+  //   var alist = document.querySelectorAll('a');
+  //   var i = 0;
+  //   while(i < alist.length){
+  //     alist[i].style.color = color;
+  //     i++;
+  //   }
+
+  // jquery 사용후 반복문 제거가능.
+  $('a').css("color",color);
+  }
+
+}
+var Switches = {
+  setValue:function(value){
+    // var slist = document.querySelectorAll('.switch')
+    // var i = 0;
+    // while(i < slist.length){
+    //   slist[i].value = value;
+    //   i++;
+    // }
+  $('.switch').val(value);
+  }
+}
+function changeSkinMode(self){
+  var target=document.querySelector('body');
+  if(self.value=='day'){
+    Body.setColor('white');
+    Body.setBackgroundColor('black');
+    Switches.setValue('night');
+    Links.setColor('grey');
+  }else{
+    Body.setColor('black');
+    Body.setBackgroundColor('white');
+    Switches.setValue('day');
+    Links.setColor('red');
+  }
+}
